3536fb68191116af8dac477f028c0828b511c185
German <bernhardt.german@gmail.com>
2022-03-28 21:15:38 -0300

    UI stub

---
+++ Assets/Scripts/View/EndView.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndView : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(BackToHome);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void BackToHome()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scripts/View/InGameView.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameView : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.GetComponentInChildren<Button>().onClick.AddListener(Log);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void Log()
+    {
+        Transform player1Scores = transform.Find("BoardBackground").transform.Find("PlayerScores (1)");        
+        
+        foreach (Transform child in player1Scores.transform)
+        {
+            if (child.name.Contains("TurnScoreContainer"))
+            {
+                Debug.Log(child.name);
+            }
+        }
+
+    }
+}
